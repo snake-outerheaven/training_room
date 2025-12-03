4c3a3d12c886c7c264d7f9b5ba627aa6228ce48f
neo-dev <sei133dirsoa@gmail.com>
2025-12-03 16:13:19 -0300

    Create little MVC arch dirs for ex1_5 in C#.

--- ex1_5/Program.cs
+++ ex1_5/Program.cs
@@ -1,67 +1,10 @@
-﻿class Student
+﻿using ex1_5.Student;
+
+class Program
 {
-    public Guid Id { get; } // id do aluno no sistema
-    public string Name { get; private set; } // nome do aluno
-    private List<float> _grades; // array privado de notas de cada aluno
-    public IReadOnlyList<float> Grades => _grades.AsReadOnly(); // notas encapsuladas
-
-    public float Average => _grades.Count() == 0 ? 0f : _grades.Average(); // aluno sempre terá a sua média.
-
-    public Student(string name)
+    static void Main(params string[] args)
     {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("O nome do aluno não pode ser vazio ou conter apenas espaços.");
-
-        Id = Guid.NewGuid();
-        Name = name;
-        _grades = new List<float>();
+        Student a = new Student("Daniel",[6.0f,5.4f,8.5f]);
+        Console.WriteLine(a);
     }
-
-    /*
-        Construtor sobrecarregado para permitir diversas formas de instanciar um estudante no sistema.
-    */
-    public Student(string name, List<float> grades) : this(name)
-    {
-        _grades = grades?.Where(g => !float.IsNaN(g) && g >= 0f).ToList() ?? new List<float>();
-    }
-
-    public void SetName(string name)
-    {
-        if (string.IsNullOrWhiteSpace(name))
-            throw new ArgumentException("O nome do aluno não pode ser vazio ou conter apenas espaços.");
-        Name = name;
-    }
-
-    public void AddGrade(float grade)
-    {
-        if (float.IsNaN(grade) || grade < 0)
-            throw new ArgumentException("A nota a ser adicionada não pode ser NaN!");
-        _grades.Add(grade);
-    }
-
-    public void RemoveGrade(int index)
-    {
-        if (index < 0 || index >= _grades.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), "Índice inválido!");
-        _grades.RemoveAt(index);
-    }
-
-    public void SetGrade(float grade, int index)
-    {
-        if (float.IsNaN(grade) || grade < 0)
-            throw new ArgumentException("A nota a ser digitada não pode ser NaN ou negativa!");
-        if (index < 0 || index >= _grades.Count)
-            throw new ArgumentOutOfRangeException(nameof(index), "Índice inválido!");
-        _grades[index] = grade;
-    }
-    public void SetGrade(List<float> grades)
-    {
-        _grades = grades?.Where(g => !float.IsNaN(g) && g >= 0f).ToList() ?? new List<float>();
-    }
-
-
-    public override string ToString()
-    {
-        return $"{Name} (Id: {Id}) - Média: {Average:F2}, Notas: {string.Join(", ", _grades)}";
-    }
-}
+};
