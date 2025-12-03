15eee3ec4abe359ea5df144f1eecd1588b8070dd
neo-dev <sei133dirsoa@gmail.com>
2025-12-02 23:39:42 -0300

    Create ex1_4.

--- ex1_3/Program.cs
+++ ex1_3/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Exemplo de como a linguagem suporta concatenação de strings com o operador + 
+*/
+string name1 = "Bob";
+string name2 = "Daniel";
+string output = "Bem vindos " + name1 + " e " + name2 + "!";
+Console.WriteLine(output);
+
+// o próximo exercício é somente concatenação de strings dentro do Console.WriteLine()
+Console.WriteLine(name1 + " and " + name2 + ", " + "Greetings!");
+
+
+/*
+    Agora começa a ficar interessante, $"{var1} {var2}" é como C# permite a interpolação de strings
+*/
+
+var output2 = $"Bem vindos {name1} e {name2}!"; // var para inferência de tipo pelo Roslyn
+Console.WriteLine(output2);
+
+/*
+    Combinando literais e strings interpoladas
+*/ 
+
+var output3 = $@"C:\Users\{name1}> ";
+Console.WriteLine(output3);
+
+/*
+    Desafio -> Obter a saída : 
+"    
+View English output:
+  c:\Exercise\ACME\data.txt
+
+Посмотреть русский вывод:
+  c:\Exercise\ACME\ru-RU\data.txt
+
+"
+com as variáveis fornecidas abaixo.
+*/
+
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+
+Console.WriteLine($@"
+View English Output: 
+    C:\Exercise\{projectName}\data.txt
+{russianMessage}:
+    C:\Exercise\{projectName}\ru-RU\data.txt
+");
---
+++ ex1_4/Program.cs
@@ -0,0 +1,89 @@
+﻿/*
+    Operator overloading, somando tipos diferentes, além do uso de 
+    () para controlar ordem de operações.
+*/
+
+string a = "bob";
+decimal b = 13.0m;
+Console.WriteLine(a + " sold " + (b + 7m) + " Bs.");
+
+/*
+    Operações matemáticas não são muito diferentes de C, pelo menos
+    por enquanto
+*/
+
+int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+float floatQuotient = 7.0F / 5.0F;
+double doubleQuotient = 7.0d / 5.0d;
+decimal decimalQuotient = 7.0m / 5.0m;
+
+Console.WriteLine($"Sum: {sum}.");
+Console.WriteLine($"Diff: {difference}.");
+Console.WriteLine($"Product: {product}.");
+Console.WriteLine($"Quotient: {quotient}.");
+Console.WriteLine($"Division with single precision float type number: {floatQuotient}.");
+Console.WriteLine($"Division with double precision float point number: {doubleQuotient}.");
+Console.WriteLine($"Division with a float point number from the decimal system: {decimalQuotient}.");
+
+/*
+    Agora é o type casting padrão
+*/
+
+int ab = 1;
+int bc = 2;
+decimal de = (decimal)ab / (decimal)bc;
+Console.WriteLine(de);
+
+/*
+    Operações com módulo.
+*/
+
+int abc = 7 % 3;
+Console.WriteLine("Operações com módulo\t" + abc + "\t->\t" + $"{100 % 3}");
+
+/*
+    PEMDAS
+
+    Parênteses -> Exponentes -> Multiplicação e Divisão -> Adição e Subtração.
+*/
+
+int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
+
+/*
+    C# também possui +=, -=, *=, ++,--, e eles funcionam igual C
+*/
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
+
+// PS, existe pré e pós incremento também!!
+
+/*
+    Desafio para calcular celsius dada temperatura em fahrenheit.
+*/
+
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine(celsius.ToString("F2"));
